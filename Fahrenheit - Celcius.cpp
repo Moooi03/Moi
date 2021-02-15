25930edf68737cb51d9258b6352e1413bc1e0be7
Moooi03 <78566419+Moooi03@users.noreply.github.com>
2021-02-15 17:39:00 -0600

    Add files via upload

---
+++ Fahrenheit - Celcius.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* print Fahrenheit-celcius table
+  for fahr = 0, 20, ..., 300*ç */
+  main()
+  {
+  	int fahr, celcius;
+  	int lower, upper, step;
+  	
+  	lower= 0; /*lower limit temperature table */
+  	upper = 300; /*upper limit */
+  	step = 20; /*step size*/
+  	fahr= lower;
+  	while (fahr <= upper) {
+  		celcius =5* (fahr-32)/9;
+  		printf("%d\t%d\n", fahr, celcius);
+  		fahr = fahr + step;
+  		
+	  }
+  	
+  }
