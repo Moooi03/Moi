b404e2b4dc8eb2843313a17825210389487f6dd4
Moooi03 <78566419+Moooi03@users.noreply.github.com>
2021-03-11 12:07:15 -0600

    Add files via upload

---
+++ homework/Homework3.C
@@ -0,0 +1,87 @@
+#include <stdio.h>          // I received help from Raul :) //
+
+#define TRUE 1
+#define FALSE 0
+
+int main()
+{
+int c, number = FALSE, parenthesis = FALSE, quotes = FALSE;
+while ((c = getchar()) != EOF)
+{
+switch(c)
+{
+      case '(':
+          parenthesis=TRUE;
+          putchar(c);
+          break;
+      case ')':
+          parenthesis=FALSE;
+          putchar(c);
+          break;
+      case '[':
+          parenthesis=TRUE;
+          putchar(c);
+          break;
+      case ']':
+          parenthesis=FALSE;
+          putchar(c);
+          break;
+      case '<':
+          parenthesis=TRUE;
+          putchar(c);
+          break;
+      case '>':
+          parenthesis=FALSE;
+          putchar(c);
+          break;
+      case '{':
+          parenthesis=TRUE;
+          putchar(c);
+          break;
+      case '}':
+          parenthesis=FALSE;
+          putchar(c);
+          break;
+      case '\'':
+          putchar(c);
+          quotes++;
+          break;
+
+       case '\"':
+          putchar(c);
+          quotes++;
+          break;
+
+      default:
+        if(c!='.'&&c!=':'&&c!=';'&&c!='-'&&c!='_'&&c!='!'&&c!='\?')
+            {
+
+            if (parenthesis==TRUE||quotes==TRUE){
+            putchar(c);
+
+            }
+             else
+            {
+             if (quotes==2)
+                {
+                    quotes=0;
+                }
+                if (c=='1'||c=='2'||c=='3'||c=='4'||c=='5'||c=='6'||c=='7'||c=='8'||c=='9'||c=='0'){
+                    number++;
+                }
+                else
+                {
+                  number=0;
+                }
+                if (number==1){
+                    putchar('X');
+                }
+                if (number==0){
+                    putchar(c);
+             }
+            }
+           }
+          }
+         }
+    return 0;
+  }
